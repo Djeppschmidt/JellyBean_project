2cdafa916cd3231f4c65881e742333ec278dd0bc
Dietrich Epp Schmidt <djeppschmidt@gmail.com>
2018-03-08 15:35:10 -0500

    prep for class
    updated with outputs to be used for the class exercise. the OTU table is based on a normal distribution model for randomly generated portion. Should be updated with a quasi-gaussian model

--- Generation_Workflow.R
+++ Generation_Workflow.R
@@ -132,14 +132,136 @@ spp35<-function(a,b,c,d,e) {abun<-10*c+0*b*a*d*e
 
 fun<-c(spp1,spp2,spp3,spp4,spp5,spp6,spp7,spp8,spp9,spp10,spp11,spp12,spp13,spp14,spp15,spp16,spp17,spp18,spp19,spp20,spp21,spp22,spp23,spp24,spp25,spp26,spp27,spp28,spp29,spp30,spp31,spp32,spp33,spp34,spp35)
 
+#make environmental table
+set.seed(019283743)
+#conditionsF1 (eg som)
+library(reshape2)
+f1c1<-c(5,5,5,5,5,5)
+f1c2<-c(1,3,10,15,3,15)
+f1c3<-c(0.5,0.5,3,3,1,5)
+F1.frame<-mapply(rnorm, f1c1,f1c2,f1c3)
+F1<-melt(F1.frame)
 
+#F2
+f2c1<-c(5,5,5,5,5,5)
+f2c2<-c(34,30,50,55,35,60)
+f2c3<-c(0.5,0.5,3,3,1,5)
+F2.frame<-mapply(rnorm, f2c1,f2c2,f2c3)
+F2<-melt(F2.frame)
+
+#F3
+f3c1<-c(5,5,5,5,5,5)
+f3c2<-c(1,3,10,15,3,15)
+f3c3<-c(0.5,0.5,3,3,1,5)
+F3.frame<-mapply(rnorm, f3c1,f3c2,f3c3)
+F3<-melt(F3.frame)
+
+#F4
+f4c1<-c(5,5,5,5,5,5)
+f4c2<-c(1,3,10,15,3,15)
+f4c3<-c(0.5,0.5,3,3,1,5)
+F4.frame<-mapply(rnorm, f4c1,f4c2,f4c3)
+F4<-melt(F4.frame)
+
+#F5
+f5c1<-c(5,5,5,5,5,5)
+f5c2<-c(1,3,10,15,3,15)
+f5c3<-c(0.5,0.5,3,3,1,5)
+F5.frame<-mapply(rnorm, f5c1,f5c2,f5c3)
+F5<-melt(F5.frame)
+
+Factors<-data.frame(F1$value,F2$value,F3$value,F4$value,F5$value)
+rownames(Factors)<-Sites
+colnames(Factors)<-c("F1","F2","F3","F4","F5")
+head(Factors)
 #### output response table###
 out<-matrix(data=NA, nrow=nrow(Factors), ncol = length(fun))
 
-out<-for(i in 1:length(fun)) {
-  	for(row in 1:nrow(Factors)){
-   rt[row,i]<-do.call(fun[[i]], list(Factors[row,1],Factors[row,2],Factors[row,3],Factors[row,4],Factors[row,5]))
+for(i in 1:length(fun)) {
+  for(row in 1:nrow(Factors)){
+   out[row,i]<-do.call(fun[[i]], list(Factors[row,1],Factors[row,2],Factors[row,3],Factors[row,4],Factors[row,5]))
       }
 }
 
-rt
+Sites<-c("Site1","Site2","Site3","Site4","Site5","Site6","Site7","Site8","Site9","Site10","Site11","Site12","Site13","Site14","Site15","Site16","Site17","Site18","Site19","Site20","Site21","Site22","Site23","Site24","Site25","Site26","Site27","Site28","Site29","Site30")
+Taxa<-c("spp1","spp2","spp3","spp4","spp5","spp6","spp7","spp8","spp9","spp10","spp11","spp12","spp13","spp14","spp15","spp16","spp17","spp18","spp19","spp20","spp21","spp22","spp23","spp24","spp25","spp26","spp27","spp28","spp29","spp30","spp31","spp32","spp33","spp34","spp35")
+
+row.names(out)<-Sites
+colnames(out)<-Taxa
+
+head(out)
+
+
+### convert negative values to 0
+out[out<0]<-0
+#out
+### round numbers
+out<-round(out)
+head(out)
+
+#save as phyloseq table
+
+library(phyloseq)
+OTU<-otu_table(out, taxa_are_rows = FALSE)
+Sa<-sample_data(Factors)
+NormdistCommPS<-phyloseq(OTU, Sa)
+NormdistCommPS
+
+saveRDS(NormdistCommPS, "~/Documents/GitHub/JellyBean_Project/RefCommNormDist.rds")
+
+
+rrarefy2<- function (x, sample, replace) 
+{
+  if (!identical(all.equal(x, round(x)), TRUE)) 
+    stop("function is meaningful only for integers (counts)")
+  x <- as.matrix(x)
+  if (ncol(x) == 1) 
+    x <- t(x)
+  if (length(sample) > 1 && length(sample) != nrow(x)) 
+    stop(gettextf("length of 'sample' and number of rows of 'x' do not match"))
+  sample <- rep(sample, length = nrow(x))
+  colnames(x) <- colnames(x, do.NULL = FALSE)
+  nm <- colnames(x)
+  if (any(rowSums(x) < sample)) 
+    warning("Some row sums < 'sample' and are not rarefied")
+  for (i in 1:nrow(x)) {
+    if (sum(x[i, ]) <= sample[i]) 
+      next
+    row <- sample(rep(nm, times = x[i, ]), sample[i], replace)
+    row <- table(row)
+    ind <- names(row)
+    x[i, ] <- 0
+    x[i, ind] <- row
+  }
+  x
+}
+#define rarefaction depth for each sample
+#may need to repeat this step multiple times to generate no negative numbers
+depth<-list(rnorm(30, 80000, 40000))
+depth<-round(depth[[1]])
+depth
+
+#subsampling; rarefying without replacement assumes that the community is completely sampled 
+#(there is insufficient primer and polymerase to amplify all genes) during the library prep steps
+#subsampling; rarefying with replacement assumes that the community is not completely sampled during the library prep step
+
+rareplaced<-rrarefy2(out, depth, replace=TRUE)
+head(rareplaced)
+
+rarefied<-rrarefy2(out, depth, replace=FALSE)
+head(rarefied)
+
+sum(as.data.frame(t(rarefied))$Site1)
+sum(as.data.frame(t(rareplaced))$Site1)
+
+
+rarefied<-otu_table(rarefied, taxa_are_rows = FALSE)
+rare.NormdistCommPS<-phyloseq(rarefied, Sa)
+rare.NormdistCommPS
+
+rareplaced<-otu_table(rareplaced, taxa_are_rows = FALSE)
+replaced.NormdistCommPS<-phyloseq(rareplaced, Sa)
+replaced.NormdistCommPS
+
+saveRDS(rare.NormdistCommPS, "~/Documents/GitHub/JellyBean_Project/rarefied_com.rds")
+saveRDS(replaced.NormdistCommPS, "~/Documents/GitHub/JellyBean_Project/rareplaced_com.rds")
