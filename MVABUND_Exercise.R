0a790a98a4f66ad2123e0c78c4695af6b6a39030
Dietrich Epp Schmidt <djeppschmidt@gmail.com>
2018-03-09 15:34:08 -0500

    complete tutorial

--- MVABUND_Exercise.R
+++ MVABUND_Exercise.R
@@ -1,5 +1,5 @@
 #Learn MVABUND
-
+#download rarefied_com.rds from https://github.com/Djeppschmidt/JellyBean_project
 data<-readRDS("~/File.path/to/rarefied_com.rds")
 
 ### first, let's explore the data###
@@ -12,7 +12,7 @@ data<-readRDS("~/File.path/to/rarefied_com.rds")
 
 #What might we use to standardize this dataset?
 
-##transform sample counts 
+##transform sample counts
 #first, define the transformation factor
 
 adjustFactor<-(sum(total_abund)/length(total_abund))/total_abund
@@ -23,8 +23,8 @@ data2<-as.matrix(otu_table(data))*adjustFactor
 data2<-round(data2)
 
 #replace otu table in phyloseq object
-otu_table(data)<-otu_table(data2, taxa_are_rows = FALSE)
-       
+otu_table(data2)<-otu_table(data2, taxa_are_rows = FALSE)
+
 #Check your OTU table output . Does it look reasonable? are there decimals?(answer should be no!)
 
 
@@ -36,4 +36,29 @@ otu_table(data)<-otu_table(data2, taxa_are_rows = FALSE)
 library(mvabund)
 
 
+#check the abundance distri ution of species...
+boxplot(otu_table(data2), horizontal = T, main="Abundance")
+#check the mean variance relationship...
+meanvar.plot(otu_table(data2))
 
+#build a statistical model for your
+?manyglm
+manyglm
+mod1<-manyglm(otu_table(data2)~sample_data(data2)$F1, family="poisson")
+mod2<-manyglm(otu_table(data2)~sample_data(data2)$F1+sample_data(data2)$F2+sample_data(data2)$F3+sample_data(data2)$F4+sample_data(data2)$F5, family="poisson")
+
+#examine residual plot
+plot(mod1)
+plot(mod2)
+
+#try another distribution
+mod3<-manyglm(otu_table(data2)~sample_data(data2)$F1, family="negative_binomial")
+plot(mod3)
+mod4<-manyglm(otu_table(data2)~sample_data(data2)$F1+sample_data(data2)$F2+sample_data(data2)$F3+sample_data(data2)$F4+sample_data(data2)$F5, family="negative_binomial")
+plot(mod4)
+
+#run statistical test
+anova(mod3)
+
+#run test on each species individually
+anova(mod3, p.uni="adjusted")
