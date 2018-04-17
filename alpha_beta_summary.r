38d069cf241f80c31c6c46a4df9e1de05d6d1da2
Dietrich Epp Schmidt <djeppschmidt@gmail.com>
2018-04-17 14:11:36 -0400

    updates

--- alpha_beta_summary.r
+++ alpha_beta_summary.r
@@ -4,17 +4,41 @@ com<-readRDS("path/to/file.rds")
 
 ### alpha diversity
 alpha<-estimate_richness(com, measures = "Observed")
+?estimate_richness
+categories<-c(rep(1,5), rep(2,5), rep(3,5), rep(4,5), rep(5,5), rep(6,5))
+summary(aov(alpha$Observed~categories
+            
+            ))
+?aov        
 
-### beta diversity
+boxplot(alpha$Observed~categories)
+        
+
+)### beta diversity
 # bray-curtis
 dist<-vegdist(otu_table(com), method="bray")
-
+?vegdist
 # Permanova
 
 ?adonis
-adonis(dist~category+f1+f2+f3+f4+f5, com)
-# ordination
+adonis(dist~categories+F1+F2+F3+F4+F5, data.frame(sample_data(com))) 
+?adonis
+sample_data(com)
 
-ord<-metaMDS(com, distance-"bray")
+#ordination
+
+ord<-metaMDS(otu_table(com), distance="bray")
+plot(ord, col= csample_data(com)$categories)
+
+?plot
+
+?plot_ordination
+sample_data(com)$categories<-categories
+plotord = plot_ordination(com, ord, color="categories")+scale_shape_identity()
+plotord
 
 fit<-envfit(ord, sample_data(com))
+
+plot(ord)
+plot(fit, add=TRUE)
+
